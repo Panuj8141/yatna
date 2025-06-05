e8b3abff971ee17cdaa6d410ffeba9df015ee635
Panuj8141 <153091128+Panuj8141@users.noreply.github.com>
2025-06-05 18:06:07 +0530

    Update script.js
    updating the path for service worker

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const getData = async (path)=>{
 
   if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('/yatna/service-worker.js').then(registration => {
       console.log('Service Worker registered with scope:', registration.scope);
     }).catch(error => {
       console.log('Service Worker registration failed:', error);
