fe816a918a94ed777fdeef5c932dd3cf0dc7f04d
Panuj8141 <153091128+Panuj8141@users.noreply.github.com>
2025-06-05 17:30:26 +0530

    Update script.js
    adding the register for service workers

--- script.js
+++ script.js
@@ -61,4 +61,15 @@ const getData = async (path)=>{
 
   lodaer();
   // Initial load
+
+
+  if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(error => {
+      console.log('Service Worker registration failed:', error);
+    });
+  });
+}
   
